1932319dd139ff863a56189c1afc88469b044f9c
shahhrahman <shahhabibur1@gmail.com>
2019-01-27 14:04:51 +0600

    Assign sprite to player and give its properties

--- client/state/player/createPlayer.js
+++ client/state/player/createPlayer.js
@@ -1,5 +1,5 @@
 const createPlayer = (x, y, game) => {
-    const sprite = game.add.sprite(x, y, 'car')
+    const sprite = game.add.sprite(x, y, 'swan')
     game.physics.p2.enable(sprite, false)
     return sprite
   }
